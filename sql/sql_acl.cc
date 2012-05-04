a5422f2a5a4ed9e7b5ede8848bd95864c1076988
Steaphan Greene <steaphan@fb.com>
2012-05-04 16:27:05 -0700

    Correct basic warnings from official MySQL-5.6
    Summary:
    WebScaleSQL Feature: Basic Code Fixes
    
    Corrected all of the gcc and clang warnings from upstream MySQL source.
    Our MySQL-5.6 now builds both ways with zero warnings in both.
    
    Did not fix intentional warnings which are fixed when it is
    built in maintainer mode (like at: sql/sql_planner.cc:110).
    
    Oracle's code was missing a "using" for "isfinite", needed in C++11.
    
    Oracle blindly defined _GNU_SOURCE, which may already be defined.
    
    Oracle blindly defined bzero, which may already be a macro.
    
    Fixed some bit-size mismatches in bitwise operations, detected by clang.
    
    Fixed some integer/boolean type mismatches, detected by clang.
    
    Fixed an uninitialized variable.
    
    Removed some truly unused variables.
    
    Catch an fread() return value, which was ignored.
    
    The var "thread_running" was renamed to "num_thread_running" in 5.6.6,
    but apparently not all the uses of "thread_running" were fixed -
    the extern still had the old name.  This fixes that.
    
    Test Plan:
    Built both build types with devserver builds, no warnings.
    Built both build types with Jenkins builds, no warnings.
    
    Also tested building with:
    * clang-3.5 with "-Wno-deprecated-register".
    * gcc-4.9
    
    Reviewers: flamingcow, pivanof, jeremycole, andrew-ford, inaam-rana, weixiang.zhai
    
    Reviewed By: pivanof, weixiang.zhai
    
    CC: jtolmer, MarkCallaghan
    
    Differential Revision: https://reviews.facebook.net/D15663
    Differential Revision: https://reviews.facebook.net/D15897
    Differential Revision: https://reviews.facebook.net/D16497
    Differential Revision: https://reviews.facebook.net/D36147

--- sql/sql_acl.cc
+++ sql/sql_acl.cc
@@ -3013,8 +3013,8 @@ static int replace_user_table(THD *thd, TABLE *table, LEX_USER *combo,
 	 if old_passwords == 2, error.
       * An empty password is considered to be of mysql_native type.
     */
-    
-    if (combo->plugin.str == NULL || combo->plugin.str == '\0')
+
+    if (combo->plugin.str == NULL || combo->plugin.str[0] == '\0')
     {
       if (combo->uses_identified_by_password_clause)
       {
@@ -9114,7 +9114,7 @@ void fill_effective_table_privileges(THD *thd, GRANT_INFO *grant,
   /* global privileges */
   grant->privilege= sctx->master_access;
 
-  if (!sctx->priv_user)
+  if (sctx->priv_user[0] == '\0')
   {
     DBUG_PRINT("info", ("privilege 0x%lx", grant->privilege));
     DBUG_VOID_RETURN;                         // it is slave
@@ -11599,7 +11599,12 @@ private:
         filesize= ftell(key_file);
         fseek(key_file, 0, SEEK_SET);
         *key_text_buffer= new char[filesize+1];
-        (void) fread(*key_text_buffer, filesize, 1, key_file);
+        if(fread(*key_text_buffer, filesize, 1, key_file) != 1)
+        {
+          sql_print_error("Failure in fread() of key_text_buffer.");
+          fclose(key_file);
+          return true;
+        }
         (*key_text_buffer)[filesize]= '\0';
       }
       fclose(key_file);

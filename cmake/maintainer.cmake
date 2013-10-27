e296183587947bca4ac662639e66792b9b8c92a9
Steaphan Greene <steaphan@fb.com>
2013-10-26 21:33:32 -0700

    Switch to C++11 and C99
    Summary:
    Feature: Basic Build Fixes
    
    Adds -std=c++11 to gcc C++ builds, and -std=c99 to gcc C builds.
    
    Also, -Wdeclaration-after-statement isn't compatible with C99 (since
    it's a warning about a feature now allowed in C99), so this diff also
    removes this.
    
    Test Plan: Built with it, with this option removed from our build tools.
    
    Reviewers: flamingcow, pivanof, jeremycole, andrew-ford
    
    Reviewed By: pivanof
    
    CC: jtolmer, MarkCallaghan
    
    Differential Revision: https://reviews.facebook.net/D14835

--- cmake/maintainer.cmake
+++ cmake/maintainer.cmake
@@ -20,12 +20,6 @@ MACRO(SET_MYSQL_MAINTAINER_GNU_C_OPTIONS)
   SET(MY_MAINTAINER_WARNINGS
       "-Wall -Wextra -Wunused -Wwrite-strings -Wno-strict-aliasing  -Werror")
 
-  CHECK_C_COMPILER_FLAG("-Wdeclaration-after-statement"
-                        HAVE_DECLARATION_AFTER_STATEMENT)
-  IF(HAVE_DECLARATION_AFTER_STATEMENT)
-    SET(MY_MAINTAINER_DECLARATION_AFTER_STATEMENT
-        "-Wdeclaration-after-statement")
-  ENDIF()
   SET(MY_MAINTAINER_C_WARNINGS
       "${MY_MAINTAINER_WARNINGS} ${MY_MAINTAINER_DECLARATION_AFTER_STATEMENT}"
       CACHE STRING "C warning options used in maintainer builds.")
@@ -37,12 +31,6 @@ MACRO(SET_MYSQL_MAINTAINER_GNU_C_OPTIONS_NO_WERROR)
   SET(MY_MAINTAINER_WARNINGS
       "-Wall -Wextra -Wunused -Wwrite-strings -Wno-strict-aliasing")
 
-  CHECK_C_COMPILER_FLAG("-Wdeclaration-after-statement"
-                        HAVE_DECLARATION_AFTER_STATEMENT)
-  IF(HAVE_DECLARATION_AFTER_STATEMENT)
-    SET(MY_MAINTAINER_DECLARATION_AFTER_STATEMENT
-        "-Wdeclaration-after-statement")
-  ENDIF()
   SET(MY_MAINTAINER_C_WARNINGS
       "${MY_MAINTAINER_WARNINGS} ${MY_MAINTAINER_DECLARATION_AFTER_STATEMENT}"
       CACHE STRING "C warning options used in maintainer builds.")

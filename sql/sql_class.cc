b88ae6fd2e5fd166cc09311eb8a74f99c6f2da1a
Steaphan Greene <steaphan@fb.com>
2014-03-17 15:57:29 -0700

    Support millisecond timeouts for reads, writes, and connects
    Summary:
    WebScaleSQL Feature: Millisecond Client Timeouts
    
    This diff exposes three new client options:
    
    MYSQL_OPT_CONNECT_TIMEOUT_MS
    MYSQL_OPT_READ_TIMEOUT_MS
    MYSQL_OPT_WRITE_TIMEOUT_MS
    
    Which are similar to the non-_MS options, except the value is, of
    course, in milliseconds.
    
    This diff also changes a number of timeout-related codepaths to use a
    structure rather than a naked integer.  This helps prevent many, many
    classes of errors that come from accidentally multiplying or dividing by
    1000 to convert (or forgetting to), and creates a form of type safety
    for timeouts.
    
    Test Plan: jenkins, mtr
    
    Reviewers: pivanof, inaam-rana, darnaut, weixiang.zhai
    
    Reviewed By: darnaut, inaam-rana, weixiang.zhai, pivanof
    
    CC: jtolmer, MarkCallaghan, flamingcow, jeremycole, andrew-ford, liang.guo.752, chip, atcurtis, jtolmer, CalvinSun, WebScaleSQL
    
    Differential Revision: https://reviews.facebook.net/D17019
    Differential Revision: https://reviews.facebook.net/D17667
    Differential Revision: https://reviews.facebook.net/D32811
    Differential Revision: https://reviews.facebook.net/D35571

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -253,7 +253,7 @@ PSI_thread *thd_get_psi(THD *thd)
 */
 ulong thd_get_net_wait_timeout(THD* thd)
 {
-  return thd->variables.net_wait_timeout;
+  return thd->variables.net_wait_timeout_seconds;
 }
 
 /**

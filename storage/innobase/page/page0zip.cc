82de40da4c55bdb794089b0700d99ae96d30a1cf
Steaphan Greene <steaphan@fb.com>
2014-04-10 11:56:34 -0700

    Allow reading of headerless compressed pages
    Summary:
    Feature: 2012 InnoDB Compression
    
    The complete version of 2012 compression writes headerless zlib data
    blobs.  This is true in both the 5.1 and 5.6 Facebook branches.
    
    This change allows WebScaleSQL to read those pages, in addition to
    those with headers.
    
    But, this still writes only with zlib headers, not without.
    
    A later diff will add support to WebScaleSQL for actually writing
    these headerless pages.
    
    Needed to backport inflateReset2() from a future version of zlib in
    order to reset a decompression stream without allocating memory for its
    internal state and with specifying windowBits. This was necessary to
    automatically determine whether a compressed page was compressed using
    adler32 checksums or not. There should not be any issues in terms of
    backwards compatibility with this. After this, a clean build is required
    for stress tests to pass.
    
    Test Plan: Tested reading data compressed by the FB branch.  Worked.
    
    Reviewers: pivanof, inaam-rana
    
    Reviewed By: inaam-rana
    
    Subscribers: WebScaleSQL, jtolmer
    
    Differential Revision: https://reviews.facebook.net/D17691

--- storage/innobase/page/page0zip.cc
+++ storage/innobase/page/page0zip.cc
@@ -2942,6 +2942,64 @@ zlib_done:
 }
 
 /**********************************************************************//**
+This function determines the sign for window_bits and reads the zlib header
+from the decompress stream. The data may have been compressed with a negative
+(no adler32 headers) or a positive (with adler32 headers) window_bits.
+Regardless of the current value of page_zip_zlib_wrap, we always
+first try the positive window_bits then negative window_bits, because the
+surest way to determine if the stream has adler32 headers is to see if the
+stream begins with the zlib header together with the adler32 value of it.
+This adds a tiny bit of overhead for the pages that were compressed without
+adler32s.
+@return TRUE if stream is initialized and zlib header was read, FALSE
+if data can be decompressed with neither window_bits nor -window_bits */
+UNIV_INTERN
+ibool
+page_zip_init_d_stream(
+	z_stream* strm,
+	int window_bits,
+	ibool read_zlib_header)
+{
+	/* Save initial stream position, in case a reset is required. */
+	Bytef* next_in = strm->next_in;
+	Bytef* next_out = strm->next_out;
+	ulint avail_in = strm->avail_in;
+	ulint avail_out = strm->avail_out;
+
+	if (UNIV_UNLIKELY(inflateInit2(strm, window_bits) != Z_OK)) {
+		/* init must always succeed regardless of window_bits */
+		ut_error;
+	}
+	/* Try decoding a zlib header assuming adler32. */
+	if (inflate(strm, Z_BLOCK) == Z_OK)
+		/* A valid header was found, all is well.  So, we return
+		   with the stream positioned just after this header. */
+		return(TRUE);
+
+	/* A valid header was not found, so now we need to re-try this
+	   read assuming there is *no* header (negative window_bits).
+           So, we need to reset the stream to the original position,
+           and change the window_bits to negative, with inflateReset2(). */
+	strm->next_in = next_in;
+	strm->next_out = next_out;
+	strm->avail_in = avail_in;
+	strm->avail_out = avail_out;
+	if (UNIV_UNLIKELY(inflateReset2(strm, -window_bits) != Z_OK)) {
+		/* init must always succeed regardless of window_bits */
+		ut_error;
+	}
+
+	if (read_zlib_header) {
+		/* No valid header was found, and we still want the header
+                   read to have happened, with negative window_bits. */
+		return(inflate(strm, Z_BLOCK) == Z_OK);
+	}
+	/* Did not find a header, but didn't require one, so just return
+           with the stream position reset to where it originally was. */
+	return(TRUE);
+}
+
+/**********************************************************************//**
 Decompress a page.  This function should tolerate errors on the compressed
 page.  Instead of letting assertions fail, it will return FALSE if an
 inconsistency is detected.
@@ -3052,13 +3110,7 @@ zlib_error:
 	d_stream.next_out = page + PAGE_ZIP_START;
 	d_stream.avail_out = UNIV_PAGE_SIZE - PAGE_ZIP_START;
 
-	if (UNIV_UNLIKELY(inflateInit2(&d_stream, UNIV_PAGE_SIZE_SHIFT)
-			  != Z_OK)) {
-		ut_error;
-	}
-
-	/* Decode the zlib header and the index information. */
-	if (UNIV_UNLIKELY(inflate(&d_stream, Z_BLOCK) != Z_OK)) {
+	if (!page_zip_init_d_stream(&d_stream, UNIV_PAGE_SIZE_SHIFT, TRUE)) {
 
 		page_zip_fail(("page_zip_decompress:"
 			       " 1 inflate(Z_BLOCK)=%s\n", d_stream.msg));

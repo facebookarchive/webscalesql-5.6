74cf15cbd36c93537e3ce16486600ef410c9f764
Steaphan Greene <steaphan@fb.com>
2014-04-10 11:56:34 -0700

    Allow reading of headerless compressed pages
    Summary:
    Feature: 2012 InnoDB Compression
    
    The complete version of 2012 compression writes headerless zlib data
    blobs.  This is true in both the 5.1 and 5.6 Facebook branches.
    
    This change allows WebScaleSQL to read those pages, in addition to
    those with headers.
    
    But, this still writes only with zlib headers, not without.
    
    A later diff will add support to WebScaleSQL for actually writing
    these headerless pages.
    
    Needed to backport inflateReset2() from a future version of zlib in
    order to reset a decompression stream without allocating memory for its
    internal state and with specifying windowBits. This was necessary to
    automatically determine whether a compressed page was compressed using
    adler32 checksums or not. There should not be any issues in terms of
    backwards compatibility with this. After this, a clean build is required
    for stress tests to pass.
    
    Test Plan: Tested reading data compressed by the FB branch.  Worked.
    
    Reviewers: pivanof, inaam-rana
    
    Reviewed By: inaam-rana
    
    Subscribers: WebScaleSQL, jtolmer
    
    Differential Revision: https://reviews.facebook.net/D17691

--- storage/innobase/btr/btr0cur.cc
+++ storage/innobase/btr/btr0cur.cc
@@ -5312,6 +5312,7 @@ btr_copy_zblob_prefix(
 	mem_heap_t*	heap;
 	int		err;
 	z_stream	d_stream;
+	ibool		inflate_inited = FALSE;
 
 	d_stream.next_out = buf;
 	d_stream.avail_out = static_cast<uInt>(len);
@@ -5327,9 +5328,6 @@ btr_copy_zblob_prefix(
 	ut_ad(zip_size >= UNIV_ZIP_SIZE_MIN);
 	ut_ad(zip_size <= UNIV_ZIP_SIZE_MAX);
 	ut_ad(space_id);
-
-	err = inflateInit(&d_stream);
-	ut_a(err == Z_OK);
 
 	for (;;) {
 		buf_page_t*	bpage;
@@ -5377,6 +5375,13 @@ btr_copy_zblob_prefix(
 
 		d_stream.next_in = bpage->zip.data + offset;
 		d_stream.avail_in = static_cast<uInt>(zip_size - offset);
+
+		if (!inflate_inited) {
+			inflate_inited = page_zip_init_d_stream(&d_stream,
+				15 /* Default windowBits for zlib */,
+				FALSE);
+			ut_a(inflate_inited);
+		}
 
 		err = inflate(&d_stream, Z_NO_FLUSH);
 		switch (err) {

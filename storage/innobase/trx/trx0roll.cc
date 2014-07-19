156f4db5c5ac357ff943717eaa618a9fe3ea2bd6
Inaam Rana <irana@twitter.com>
2014-07-18 17:57:02 -0700

    Inaccurate handling of srv_activity_count
    Summary:
    Feature: Logic error
    
    This is a fix for upstream bug#72137
    
    InnoDB increments srv_activity_count during commit and prepare.
    The purpose of increment is to tell the background threads that
    there may be some work for them to do.
    A read only transaction e.g.: an autocommit select won't make
    any changes that would require background threads to wake up.
    This spurious wakeup and increment can actually lead to
    unexpected behaviour. For example, the page_cleaner thread
    will consider that the server is active and won't go in
    background/idle flushing mode. Due to this even if there are
    dirty pages in the buffer pool it won't flush them unless
    necessitated by adaptive flushing.
    The fix is to increment activity count during commit iff
    the trx has modified something.
    
    Test Plan: mtr
    
    Reviewers: pivanof, darnaut, steaphan
    
    Reviewed By: steaphan
    
    Subscribers: CalvinSun, jtolmer, WebScaleSQL
    
    Differential Revision: https://reviews.facebook.net/D20235

--- storage/innobase/trx/trx0roll.cc
+++ storage/innobase/trx/trx0roll.cc
@@ -126,6 +126,8 @@ trx_rollback_to_savepoint_low(
 
 	mem_heap_free(heap);
 
+	srv_active_wake_master_thread();
+
 	MONITOR_DEC(MONITOR_TRX_ACTIVE);
 }
 
@@ -143,19 +145,9 @@ trx_rollback_to_savepoint(
 {
 	ut_ad(!trx_mutex_own(trx));
 
-	/* Tell Innobase server that there might be work for
-	utility threads: */
-
-	srv_active_wake_master_thread();
-
 	trx_start_if_not_started_xa(trx);
 
 	trx_rollback_to_savepoint_low(trx, savept);
-
-	/* Tell Innobase server that there might be work for
-	utility threads: */
-
-	srv_active_wake_master_thread();
 
 	return(trx->error_state);
 }
@@ -169,8 +161,6 @@ trx_rollback_for_mysql_low(
 /*=======================*/
 	trx_t*	trx)	/*!< in/out: transaction */
 {
-	srv_active_wake_master_thread();
-
 	trx->op_info = "rollback";
 
 	/* If we are doing the XA recovery of prepared transactions,
@@ -183,8 +173,6 @@ trx_rollback_for_mysql_low(
 	trx->op_info = "";
 
 	ut_a(trx->error_state == DB_SUCCESS);
-
-	srv_active_wake_master_thread();
 
 	return(trx->error_state);
 }

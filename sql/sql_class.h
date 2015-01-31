a7ed5a71a17c0982288f9adf45ca4eac0fc0fa78
Inaam Rana <irana@twitter.com>
2015-01-30 23:44:38 -0500

    Implementation of server-side statement timeout
    Summary:
    WebScaleSQL Feature: Server-Side Statement Timeout
    
    This feature was originally written by Davi Arnaut for MySQL-5.5
    at Twitter. Subsequently, this patch was accepted by upstream
    and included in 5.7. This is a back port of 5.7 patch.
    
    The implementation details and the difference between the two patches
    are explained at:
    http://mysqlserverteam.com/server-side-select-statement-timeouts/
    
    The commit message of the upstream patch that this is back port of:
      revno: 7594
      revision-id: praveenkumar.hulakund@oracle.com-20140228072806-nx118y84ff3qsia2
      parent: satya.bodapati@oracle.com-20140228072105-arj6sojckfy5goqi
      committer: Praveenkumar Hulakund <praveenkumar.hulakund@oracle.com>
      branch nick: trunk-wl6936-new
      timestamp: Fri 2014-02-28 12:58:06 +0530
      message:
        WL#6936 - Implementation of server-side statement timeout
    
        Server-side time limit for the execution of SELECT
        statements. After completing specified amount of time, the
        statement should be aborted without affecting the session
        (connection). This feature will help in interrupting and
        aborting the SELECT statements when their execution time crosses
        specified amount of time limit instead of just waiting for those
        queries to complete.
    
        This work log addresses the bug
        16271666 - IMPLEMENTATION OF SERVER-SIDE STATEMENT TIMEOUT
                   SUPPORT
    
        Original patch for this WorkLog is Contributed by "Davi Arnaut"
        and it has been developed further.
    
        Davi Arnaut Contributed original patch while working in Twitter.
        Now he works for LinkedIn.
    
    Test Plan: mtr
    
    Reviewers: pivanof, darnaut, steaphan, jeremycole
    
    Reviewed By: jeremycole, steaphan
    
    Subscribers: CalvinSun, jtolmer
    
    Differential Revision: https://reviews.facebook.net/D22881
    Differential Revision: https://reviews.facebook.net/D32769
    Differential Revision: https://reviews.facebook.net/D33687

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -85,6 +85,8 @@ class Sroutine_hash_entry;
 class User_level_lock;
 class user_var_entry;
 
+struct st_thd_timer;
+
 enum enum_ha_read_modes { RFIRST, RNEXT, RPREV, RLAST, RKEY, RNEXT_SAME };
 
 enum enum_delay_key_write { DELAY_KEY_WRITE_NONE, DELAY_KEY_WRITE_ON,
@@ -561,6 +563,8 @@ typedef struct system_variables
     'COLUMN_TYPE' field.
   */
   my_bool show_old_temporals;
+
+  ulong max_statement_time;
 } SV;
 
 
@@ -623,6 +627,11 @@ typedef struct system_status_var
 
   ulonglong bytes_received;
   ulonglong bytes_sent;
+
+  ulonglong max_statement_time_exceeded;
+  ulonglong max_statement_time_set;
+  ulonglong max_statement_time_set_failed;
+
   /*
     Number of statements sent from the client
   */
@@ -639,6 +648,7 @@ typedef struct system_status_var
   */
   double last_query_cost;
   ulonglong last_query_partial_plans;
+
 } STATUS_VAR;
 
 /*
@@ -2364,6 +2374,16 @@ public:
     return m_binlog_filter_state;
   }
 
+#ifdef HAVE_MY_TIMER
+  /** Holds active timer object */
+  struct st_thd_timer_info *timer;
+  /**
+    After resetting(cancelling) timer, current timer object is cached
+    with timer_cache timer to reuse.
+  */
+  struct st_thd_timer_info *timer_cache;
+#endif
+
 private:
   /**
     Indicate if the current statement should be discarded
@@ -3010,6 +3030,7 @@ public:
     KILL_BAD_DATA=1,
     KILL_CONNECTION=ER_SERVER_SHUTDOWN,
     KILL_QUERY=ER_QUERY_INTERRUPTED,
+    KILL_TIMEOUT=ER_QUERY_TIMEOUT,
     KILLED_NO_VALUE      /* means neither of the states */
   };
   killed_state volatile killed;
@@ -4134,7 +4155,6 @@ private:
   LEX_STRING invoker_user;
   LEX_STRING invoker_host;
 };
-
 
 /**
   A simple holder for the Prepared Statement Query_arena instance in THD.
